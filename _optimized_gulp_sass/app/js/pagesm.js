372578b724402f70e695d5aad934517cf3032690
Anna-Stoliar <Anna.S0Anna.S@gmail.com>
2018-09-18 01:38:54 +0300

    bg fixed

--- _optimized_gulp_sass/app/js/pagesm.js
+++ _optimized_gulp_sass/app/js/pagesm.js
@@ -29,26 +29,50 @@ window.onscroll = function() {
 	var scrollPos = window.pageYOffset;
   if (scrollPos < fhome) {
     document.getElementById("header-sm").style.background = "linear-gradient(to top right, #24013A, #E11CB6)";
+    $(".sm-bg-home").css( "position", "fixed");
+    $(".sm-bg-services").css( "position", "absolute");
   }
   if (scrollPos >= fhome && scrollPos < fhome + fservices) {
     document.getElementById("header-sm").style.background = "linear-gradient(to right, #4DA0B0, #D39D38)";
+    $(".sm-bg-services").css( "position", "fixed");
+    $(".sm-bg-home").css( "position", "absolute");
+    $(".sm-bg-pros").css( "position", "absolute");
   } 
   if (scrollPos >= fhome + fservices && scrollPos < fhome + fservices + fpros) {
     document.getElementById("header-sm").style.background = "linear-gradient(to right, #FCB045 0%, #FD1D1D 30%,  #AE1CE1 100%)";
+    $(".sm-bg-services").css( "position", "absolute");
+    $(".sm-bg-pros").css( "position", "fixed");
+    $(".sm-bg-faq").css( "position", "absolute");
   }
   if (scrollPos >= fhome + fservices + fpros && scrollPos < fhome + fservices + fpros + ffaq) {
     document.getElementById("header-sm").style.background = "linear-gradient(to right, #6BE585, #E72842)";
+    $(".sm-bg-team").css( "position", "absolute");
+    $(".sm-bg-pros").css( "position", "absolute");
+    $(".sm-bg-faq").css( "position", "fixed");
   } 
   if (scrollPos >= fhome + fservices + fpros + ffaq && scrollPos < fhome + fservices + fpros + ffaq + fteam) {
     document.getElementById("header-sm").style.background = "linear-gradient(to right, #544A7D, #FFD452)";
+    $(".sm-bg-faq").css( "position", "absolute");
+    $(".sm-bg-com").css( "position", "absolute");
+    $(".sm-bg-team").css( "position", "fixed");
   } 
   if (scrollPos >= fhome + fservices + fpros + ffaq + fteam && scrollPos < fhome + fservices + fpros + ffaq + fteam + fcom) {
     document.getElementById("header-sm").style.background = "linear-gradient(to right, #FF4B2B, #FF416C)";
+    $(".sm-bg-team").css( "position", "absolute");
+    $(".sm-bg-com").css( "position", "fixed");
   } 
 
 
   scrollFunction();
 }
+
+var ps1 = fhome + fservices;
+var ps2 = fhome + fservices + fpros;
+var ps3 = fhome + fservices + fpros + ffaq;
+var ps4 = fhome + fservices + fpros + ffaq + fteam;
+var ps5 = fhome + fservices + fpros + ffaq + fteam + fcom;
+
+
 
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -56,7 +80,7 @@ window.onscroll = function() {
 function scrollFunction() {
     if (document.body.scrollTop > fhome || document.documentElement.scrollTop > fhome) {
         document.getElementById("sTopBtn").style.display = "block";
-        
+
     } else {
         document.getElementById("sTopBtn").style.display = "none";
     }
